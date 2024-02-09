775431549de5166500d05f67740da8e7801b4abc
Harrison <80791359+hgreen777@users.noreply.github.com>
2024-02-09 11:48:00 +0000

    Initial Commit
    Inital Commit with CorrectTimeFormat Implemented
    BUG #1 Produced

---
+++ CS3.6-CW-Development/CS3.6-CW-Development/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim testSet As String() = {"00:00", "99:99", "19:00", "23:59", "12", "12:3123123", "James", "Ja:Me"}
+        For Each strs In testSet
+            MsgBox(strs & validation.CorrectTimeFormat(strs))
+        Next
+
+    End Sub
+End Class
---
+++ CS3.6-CW-Development/CS3.6-CW-Development/validation.vb
@@ -0,0 +1,43 @@
+﻿Public Class validation
+    ' Contains all validation modules for the program.
+    ' Ensures no code duplication and abstraction of project
+
+    'TODO : Implement Presence Check
+    ' Presence Check
+    Function DataPresent()
+
+    End Function
+
+    'TODO: Implement TimeFormat (check works)
+    ' Time Format Check 
+    Public Shared Function CorrectTimeFormat(ByVal Input As String)
+        ' Checking time is in format "HH:mm"
+
+        ' Checking the length of the string is the correct size (5 digits)
+        ' Input.Length is not 0 indexed
+        If Input.Length <> 5 Then
+            Return False
+        End If
+
+        ' Loops over each character in the string to check it meets the validation requirements
+        For i = 0 To Input.Length - 1
+            If i = 2 Then
+                ' Checks that the time is in the correct format
+                ' Checks the 3rd Character in the string is a ":"
+                ' Indexing into a string is 0 indexed
+                If Input(i) <> ":" Then
+                    Return False
+                End If
+            Else
+                ' Checks all other characters are digits.
+                ' Returns false if the character is not a digit
+                If Not (Input(i) Like "[0-9]") Then
+                    Return False
+                End If
+            End If
+        Next
+
+        ' If Function has not returned then the string has passed validation
+        Return True
+    End Function
+End Class
