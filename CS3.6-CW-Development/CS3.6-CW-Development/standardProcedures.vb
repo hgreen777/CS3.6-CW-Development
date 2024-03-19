94f7b8c663956c285cb87c43bd7c4ec795231b15
Harrison <hgreendg7@gmail.com>
2024-03-19 22:39:47 +0000

    Automatic Shifts

--- CS3.6-CW-Development/CS3.6-CW-Development/standardProcedures.vb
+++ CS3.6-CW-Development/CS3.6-CW-Development/standardProcedures.vb
@@ -7,11 +7,13 @@ Public Class standardProcedures
 
     Public Shared Sub RoundButton(btn As Button)
         'TODO: Comment Code (And sort the commented line out)
+        ' Edit button properties to make it consistent
         btn.FlatStyle = FlatStyle.Flat
         btn.FlatAppearance.BorderSize = 0
         btn.FlatAppearance.BorderColor = Color.FromArgb(191, 191, 191)
         btn.BackColor = Color.FromArgb(191, 191, 191)
 
+        ' Create a new graphics path to store the rounded edges of the button
         Dim radius As New Drawing2D.GraphicsPath
         radius.StartFigure()
 
@@ -53,69 +55,132 @@ Public Class standardProcedures
 
     'COMMENT
     Public Shared Function automaticShiftGeneration() As String '
-        ' Decare a 2d array of strings to store the shift patterns
-        Dim shiftPattern(,) As String = {{"06:00-14:00", "14:00-22:00"},     ' Monday Shifts
-                                         {"06:00-14:00", "14:00-22:00"},     ' Tuesday Shifts
-                                         {"06:00-14:00", "14:00-22:00"},     ' Wednesday Shifts
-                                         {"06:00-14:00", "14:00-22:00"},     ' Thursday Shifts
-                                         {"06:00-14:00", "14:00-21:30"},     ' Friday Shifts
-                                         {"06:30-12:30", "12:30-18:30"},     ' Saturday Shifts
-                                         {"07:00-15:00", "15:00-20:00"}}     ' Sunday Shifts
+        'Improv,ments not actually how it works. if MORE THEN 4 STAFF MEMBERS 1 HAS WEEK OFF - many drawbbacks to implementation
+        ' Last/Quick implementation to finish development so documentation is completed in time. - Therefore not efficient.
 
+        ' Decare a 2d array of strings to store the shift patterns
+        Dim shiftPattern(,) As String = {{"06:00-14:00", "06:00-14:00", "14:00-22:00", "14:00-22:00"},     ' Monday Shifts
+                                         {"06:00-14:00", "06:00-14:00", "14:00-22:00", "14:00-22:00"},     ' Tuesday Shifts
+                                         {"06:00-14:00", "06:00-14:00", "14:00-22:00", "14:00-22:00"},     ' Wednesday Shifts
+                                         {"06:00-14:00", "06:00-14:00", "14:00-22:00", "14:00-22:00"},     ' Thursday Shifts
+                                         {"06:00-14:00", "06:00-14:00", "14:00-21:30", "14:00-21:30"},     ' Friday Shifts
+                                         {"06:30-12:30", "06:30-12:30", "12:30-18:30", "12:30-18:30"},     ' Saturday Shifts
+                                         {"07:00-15:00", "07:00-15:00", "15:00-20:00", "15:00-20:00"}}     ' Sunday Shifts
+
+        ' Declare a 2d array of shifts to store the shifts created from the shift pattern. 7 days in a week and 4 shifts per day
         Dim shifts(6, 3) As Shift
-        Dim fullTimeStaff(3) As StaffMember
-        Dim offset As Integer = 0 ' Offset for shift allocation soa staff member does now work the same shift every week (read from file)
+        Dim offset As Integer = 0 ' Offset for shift allocation so staff member does now work the same shift every week (read from file) persistent value.
         FileOpen(1, "offset.txt", OpenMode.Input)
         Input(1, offset)
         FileClose(1)
 
+        'Mod by 4 as 4 shifts per day so no point shifting staff members more then 4 locations as it will just be extra processing.
         offset = offset Mod 4
         ' Shift all items in the full time staff array by the offset (looping the staff at the end to the start)
+        ' Sort out an array of staff members to be used to allocate shifts to.
+        ' Get all the full-time staff
+        Dim listOfFullTimeStaff As List(Of String) = DataStructures.StaffHashTable.fullTimeStaff()
+        ' Keep a couint of the number of full-time staff - reduce unnecessary calls to the count method.
+        Dim fullTimeStaffCount As Integer = listOfFullTimeStaff.Count
+        ' Create an array for all the list of staff
+        Dim staffArray(fullTimeStaffCount - 1) As String
+        ' Convert the list of staff to an array
+        listOfFullTimeStaff.CopyTo(staffArray)
+        ' Shuffle all staff in array to the right by x amount where x is the offset
+        Dim temp As String
+        For i = 0 To offset - 1
+            temp = staffArray(fullTimeStaffCount - 1)
+            For j = fullTimeStaffCount - 1 To 1 Step -1
+                staffArray(j) = staffArray(j - 1)
+            Next
+            staffArray(0) = temp
+        Next
+
+        ' CREATE SHIFTS.
 
 
         ' Get the current date and time
         Dim startDate As Date = Date.Now
         ' Set the start time to be the start of the day ie 00:00
         startDate = startDate.Date
-        MsgBox(startDate.ToString())
         ' Add 2 weeks to the current date
         startDate = startDate.AddDays(14)
         ' Set the start date to the start of the week it is currently in
         startDate = startDate.AddDays(-(startDate.DayOfWeek - 1))
 
-        ' Create a new shift
+        ' Create a new shift.
         Dim tmpShift As Shift
+
+        ' Starting ShiftID (Only workout once
+        ' CUZ NEED ERRORS LEAVE ID AS IS RN
+        ' HOWEVER TO FIX THE BUG OF ALL SHIFTS HAVING THE SAME ID, ONLY ALLOCATE THE ID AS THE SHIFT IS BEING ADDED TO THE LL SO THE ONE BEFORE WITH THAT ID HAS BEEN ADDED.
 
         ' For every day of the week
         For i = 0 To 6
             ' 2 shifts per day ie one early one late
-            For j = 0 To 1
-                ' So 2 staff work per slot
-                For k = 0 To 1
-                    tmpShift.shiftID = DataStructures.ShiftLL.nextAvailableID()
-                    ' Set the start date of the shift to the current date of start date but set the time to the first time in the shift pattern
-                    tmpShift.startTime = startDate.Add(TimeSpan.Parse(shiftPattern(i, j).Substring(0, 5)))
-                    ' Set the end date of the shift to the current date of start date but set the time to the second time in the shift pattern
-                    tmpShift.endTime = startDate.Add(TimeSpan.Parse(shiftPattern(i, j).Substring(6)))
-                    ' Set the staff member to be the next available staff member
-                    tmpShift.staffUserName = "HarrisonGreen0"
-                    ' Set the shift type to be a full time shift
-                    tmpShift.isTaken = False
+            For j = 0 To 3
+                ' Populate the shift with the correct shift data according to the shift pattern.
+                tmpShift.shiftID = DataStructures.ShiftLL.nextAvailableID()
+                ' Set the start date of the shift to the current date of start date but set the time to the first time in the shift pattern
+                tmpShift.startTime = startDate.Add(TimeSpan.Parse(shiftPattern(i, j).Substring(0, 5)))
+                ' Set the end date of the shift to the current date of start date but set the time to the second time in the shift pattern
+                tmpShift.endTime = startDate.Add(TimeSpan.Parse(shiftPattern(i, j).Substring(6)))
+                ' Set the staff member to be a placeholder so system does not creash - overwritten when shift allocated.
+                tmpShift.staffUserName = "HarrisonGreen0"
+                ' Set the shift type to be a full time shift
+                tmpShift.isTaken = False
 
-                    MsgBox(tmpShift.startTime.ToString())
-                    MsgBox(tmpShift.endTime.ToString())
-                    shifts(i, (j + k)) = tmpShift
-                Next
+                ' Add the shift to the array of shifts
+                shifts(i, j) = tmpShift
             Next
+            ' Increment the start date by one day so a full week of shifts are created.
             startDate = startDate.AddDays(1)
         Next
 
-        ' Get all the full-time staff
-        Dim listOfStaff As List(Of String) = DataStructures.StaffHashTable.fullTimeStaff()
-        ' Get the number of full-time staff
-        Dim fullTimeStaffCount As Integer = listOfStaff.Count
+        ' ALLOCATE SHIFTS
+
+        'TODO
         ' Calculate the number of shifts that need to be assigned - Full time staff should each have 40 hours over the week. Should not work more than 5 days in a row and should have 2 days off in a row and should not work longer then eight hours in a day.
         ' Use the number of full-time staff to calculate the number of shifts that need to be assigned and when they should be assigned given there are 4 shifts per day and 7 days in a week
+        Dim shiftsPerWeek As Integer = fullTimeStaffCount * 5
+        ' Looping over all the new shifts, allocate them to full time staff members ensuring each staff member works 5 days with 2 days of
+        For i = 0 To 6
+            Dim k As Integer
+            If fullTimeStaffCount > 4 And i > 4 Then
+                k = 4
+            Else
+                k = 0
+            End If
+            For j = 0 To 3
+                ' Check if shifts still need allocating 
+                If shiftsPerWeek > 0 Then
+                    If k < fullTimeStaffCount Then
+                        ' If the shift is not taken
+                        If shifts(i, j).isTaken = False Then
+                            ' Assign the shift to the staff member
+                            shifts(i, j).staffUserName = staffArray(k)
+                            ' Set the shift to be taken
+                            shifts(i, j).isTaken = True
+                            ' Decrement the number of shifts that need to be assigned
+                            shiftsPerWeek -= 1
+                            k += 1
+                        End If
+                    End If
+                Else
+                    ' If all shifts have been assigned then break out of the loop
+                    Exit For
+                End If
+            Next
+        Next
+
+        ' Add the shifts to the linked list of shifts
+        For i = 0 To 6
+            For j = 0 To 3
+                ' Update the shiftID to be the next available shiftID in the linked list of shifts Due to error x
+                shifts(i, j).shiftID = DataStructures.ShiftLL.nextAvailableID()
+                DataStructures.ShiftLL.append(shifts(i, j))
+            Next
+        Next
 
 
         ' Update offset
